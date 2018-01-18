b281f167ebc509f786d32b363bdbe71a5fb98469
Artem Gribanov <avgribanov@yandex-team.ru>
2018-01-18 17:27:57 +0300

    Рефакторинг кода. Вынес переменные в класс

--- autotests-tests/src/test/java/CalculatorTest.java
+++ autotests-tests/src/test/java/CalculatorTest.java
@@ -1,43 +1,23 @@
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    int a = 10;
+    int b = 17;
+    int result = 27;
+
+    Calculator calculator = new Calculator();
+
     @Test
     public void testCalculationSum() {
-        Calculator calculator = new Calculator();
-        int a = 10;
-        int b = 17;
-
-        int result = 27;
-
-        assertEquals(result, calculator.sum(a,b));
+        assertEquals(result, calculator.sum(a, b));
     }
 
     @Test
     public void testCalculationSum2() {
-        Calculator calculator = new Calculator();
-        int a = 10;
-        int b = 17;
-
-        int result = 27;
-
-        assertThat(calculator.sum(a,b), equalTo(result));
-    }
-
-    @Test
-    public void testCalculationSum3() {
-        Calculator calculator = new Calculator();
-        int a = 10;
-        int b = 17;
-
-        int result = 27;
-
-        assertThat(calculator.sum(a,b)).isEqualTo(result);
+        assertThat(calculator.sum(a, b)).isEqualTo(result);
     }
 }
